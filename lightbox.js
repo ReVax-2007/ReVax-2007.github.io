6fbfb5c79c80d14237199f83a55bf86fe74e0c79
ReVax <voet.xavier@gmail.com>
2024-12-09 16:24:33 -0500

    Pages upate

---
+++ lightbox.js
@@ -0,0 +1,5 @@
+// Ensure Bootstrap's lightbox works properly
+document.addEventListener('DOMContentLoaded', function () {
+    var lightbox = new bootstrap.Lightbox(document.querySelectorAll('[data-bs-toggle="lightbox"]'));
+  });
+  
